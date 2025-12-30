dfeaa0b91f3ff26836662bae028bd9671dd93433
xjh <nil541203@gmail.com>
2025-12-31 00:41:21 +0800

    feat: 增加 array 实现, 修改 iterator 实现

--- ds/array/array.go
+++ ds/array/array.go
@@ -1,1 +1,112 @@
 package array
+
+import (
+	"fmt"
+	"iter"
+)
+
+type Array[T any] struct {
+	data []T
+}
+
+func New[T any](size int) *Array[T] {
+	return &Array[T]{
+		data: make([]T, size, size),
+	}
+}
+
+func NewFromArray[T any](other *Array[T]) *Array[T] {
+	arr := &Array[T]{
+		data: make([]T, other.Size(), other.Size()),
+	}
+	copy(arr.data, other.data)
+	return arr
+}
+
+func (arr *Array[T]) Fill(val T) {
+	for i := range arr.data {
+		arr.data[i] = val
+	}
+}
+
+func (arr *Array[T]) Size() int {
+	return len(arr.data)
+}
+
+func (arr *Array[T]) Set(index int, val T) {
+	if index < 0 || index >= arr.Size() {
+		return
+	}
+	arr.data[index] = val
+}
+
+func (arr *Array[T]) At(index int) T {
+	if index < 0 || index >= arr.Size() {
+		panic("index out of range")
+	}
+	return arr.data[index]
+}
+
+func (arr *Array[T]) Front() T {
+	return arr.At(0)
+}
+
+func (arr *Array[T]) Back() T {
+	return arr.At(arr.Size() - 1)
+}
+
+func (arr *Array[T]) Empty() bool {
+	return len(arr.data) == 0
+}
+
+func (arr *Array[T]) SwapArray(other *Array[T]) {
+	if arr.Size() != other.Size() {
+		return
+	}
+	arr.data, other.data = other.data, arr.data
+}
+
+func (arr *Array[T]) Data() []T {
+	return arr.data
+}
+
+func (arr *Array[T]) String() string {
+	return fmt.Sprintf("%v", arr.data)
+}
+
+func (arr *Array[T]) First() *ArrayIterator[T] {
+	return arr.IterAt(0)
+}
+
+func (arr *Array[T]) Last() *ArrayIterator[T] {
+	return arr.IterAt(arr.Size() - 1)
+}
+
+func (arr *Array[T]) Begin() *ArrayIterator[T] {
+	return arr.First()
+}
+
+func (arr *Array[T]) End() *ArrayIterator[T] {
+	return arr.IterAt(arr.Size())
+}
+
+func (arr *Array[T]) IterAt(index int) *ArrayIterator[T] {
+	return &ArrayIterator[T]{
+		arr:      arr,
+		position: index,
+	}
+}
+
+func (arr *Array[T]) Values() iter.Seq[T] {
+	return arr.Begin().ToSeq()
+}
+
+func (arr *Array[T]) All() iter.Seq2[int, T] {
+	return func(yield func(int, T) bool) {
+		for i, val := range arr.data {
+			if !yield(i, val) {
+				return
+			}
+		}
+	}
+}
--- ds/array/iterator.go
+++ ds/array/iterator.go
@@ -1,1 +1,75 @@
 package array
+
+import (
+	"SPL/utils/iterator"
+	"iter"
+)
+
+type T any
+
+var _ iterator.RandomAccessIterator[T] = (*ArrayIterator[T])(nil)
+
+type ArrayIterator[T any] struct {
+	arr      *Array[T]
+	position int
+}
+
+func (iter *ArrayIterator[T]) IsValid() bool {
+	return iter.position >= 0 && iter.position < iter.arr.Size()
+}
+
+func (iter *ArrayIterator[T]) Value() T {
+	val := iter.arr.At(iter.position)
+	return val
+}
+
+func (iter *ArrayIterator[T]) Next() iterator.Cursor[T] {
+	if iter.position < iter.arr.Size() {
+		iter.position++
+	}
+	return iter
+}
+
+func (iter *ArrayIterator[T]) Clone() iterator.Cursor[T] {
+	return &ArrayIterator[T]{arr: iter.arr, position: iter.position}
+}
+
+func (iter *ArrayIterator[T]) Equal(other iterator.Cursor[T]) bool {
+	otherIter, ok := other.(*ArrayIterator[T])
+	if !ok {
+		return false
+	}
+	if otherIter.arr == iter.arr && otherIter.position == iter.position {
+		return true
+	}
+	return false
+}
+
+func (iter *ArrayIterator[T]) ToSeq() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for _, val := range iter.arr.data {
+			if !yield(val) {
+				return
+			}
+		}
+	}
+}
+
+func (iter *ArrayIterator[T]) Prev() iterator.ConstBidIterator[T] {
+	if iter.position > 0 {
+		iter.position--
+	}
+	return iter
+}
+
+func (iter *ArrayIterator[T]) SetValue(value T) {
+	iter.arr.Set(iter.position, value)
+}
+
+func (iter *ArrayIterator[T]) IteratorAt(position int) iterator.RandomAccessIterator[T] {
+	return &ArrayIterator[T]{arr: iter.arr, position: position}
+}
+
+func (iter *ArrayIterator[T]) Position() int {
+	return iter.position
+}
